b6838782582be2c44a56b877f43f5b228ef40d5b
Allen Hu <allensamuelhu@gmail.com>
2022-08-10 14:40:22 -0400

    Complete basics 2 training

---
+++ basics-2/index.js
@@ -0,0 +1,6 @@
+function displayData() {
+    alert("name: " + document.getElementById("name").value
+        + "\nemail: " + document.getElementById("name").value
+        + "\nphone: " + document.getElementById("phone").value
+        + "\nmessage: " + document.getElementById("message").value);
+  }
